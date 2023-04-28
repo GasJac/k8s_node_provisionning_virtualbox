2d6ceef6cee1ad873e4df8594f535c8644fe2c8f
gasjac <gaspardjacobson@hotmail.fr>
2023-04-29 00:11:09 +0200

    Some fix to ansible job

--- deploy.sh
+++ deploy.sh
@@ -10,24 +10,34 @@ read -p "Please enter the Ip addresse of your K8s master : " MASTER_IP
 echo $MASTER_IP;
 read -p "Please enter the User of your K8s master : " USER
 echo $USER;
+read -s -p "Please enter the Password of your K8s master : " PASS
 
 mkdir -p tools/.ssh
 
 #terraform provisioning
 cd ./terraform && bash terraform_apply.sh
 IP=`cat terraform.tfstate | grep ipv4_address | head -n1 | cut -f 2 -d ':' | cut -f 2 -d ' ' |tr ',' ' '`
+echo -e "\e[35mEnd of terraform provisioning"
 
 #for the first provisionning we need to generate a key pair to access the node
+echo -e "\e[35mGenerating key pair"
+rm ../tools/.ssh/id_rsa
+rm ../tools/.ssh/id_rsa.pub
+#ssh-keygen -R $HOST
 ssh-keygen -t rsa -f ../tools/.ssh/id_rsa -q -P ""
 HOST=`echo $IP | cut -d '"' -f 2`
-ssh-copy-id -o IdentitiesOnly=yes -i ./tools/.ssh/id_rsa.pub vagrant@$HOST
+sshpass -p "vagrant" ssh-copy-id -o IdentitiesOnly=yes -i ../tools/.ssh/id_rsa.pub vagrant@$HOST
 #and master
-ssh-copy-id -o IdentitiesOnly=yes -i ./tools/.ssh/id_rsa.pub $USER@$MASTER_IP
+sshpass -p "${PASS}" ssh-copy-id -o IdentitiesOnly=yes -i ../tools/.ssh/id_rsa.pub $USER@$MASTER_IP
 
 
 #ansible deployment
 cd ../ansible
+#backup inventory
+cp inventaire.ini inventaire.ini.backup
 #replace host with IP of the VM
-sed -i "s|worker_2_ip|${IP}|g" playbook.yml
 sed -i "s|worker_2_ip|${IP}|g" inventaire.ini
-ansible-playbook playbook.yml -i inventaire.ini --user vagrant --become
+sed -i "s|master_ip|${IP}|g" inventaire.ini
+ansible-playbook playbook.yml -i inventaire.ini --user $USER --extra-vars "ansible_sudo_pass=${PASS}"
+#replace inventory.ini
+mv inventaire.ini.backup inventaire.ini
