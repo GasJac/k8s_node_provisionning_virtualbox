b715aaa2f4d005c9fd755ba17a60bb70c2ad74f2
gasjac <gaspardjacobson@hotmail.fr>
2023-04-30 00:13:21 +0200

    Fix for ssh keygen inside bash for worker and master, fixed sed inside inventory , fixed join cluster role

--- deploy.sh
+++ deploy.sh
@@ -20,12 +20,15 @@ IP=`cat terraform.tfstate | grep ipv4_address | head -n1 | cut -f 2 -d ':' | cut
 echo -e "\e[35mEnd of terraform provisioning"
 
 #for the first provisionning we need to generate a key pair to access the node
-echo -e "\e[35mGenerating key pair"
+echo -e "\e[35mGenerating key pair\e[35m"
 rm ../tools/.ssh/id_rsa
 rm ../tools/.ssh/id_rsa.pub
-#ssh-keygen -R $HOST
-ssh-keygen -t rsa -f ../tools/.ssh/id_rsa -q -P ""
 HOST=`echo $IP | cut -d '"' -f 2`
+#ssh-keygen -R "${HOST}"
+#ssh-keygen -R "${MASTER_IP}"
+ssh-keygen -f "/home/gas/.ssh/known_hosts" -R "${HOST}"
+ssh-keygen -f "/home/gas/.ssh/known_hosts" -R "${MASTER_IP}"
+ssh-keygen -t rsa -f ../tools/.ssh/id_rsa -q -P ""
 sshpass -p "vagrant" ssh-copy-id -o IdentitiesOnly=yes -i ../tools/.ssh/id_rsa.pub vagrant@$HOST
 #and master
 sshpass -p "${PASS}" ssh-copy-id -o IdentitiesOnly=yes -i ../tools/.ssh/id_rsa.pub $USER@$MASTER_IP
@@ -36,8 +39,9 @@ cd ../ansible
 #backup inventory
 cp inventaire.ini inventaire.ini.backup
 #replace host with IP of the VM
-sed -i "s|worker_2_ip|${IP}|g" inventaire.ini
-sed -i "s|master_ip|${IP}|g" inventaire.ini
+sed -i "s|worker_2_ip|${HOST}|g" inventaire.ini
+sed -i "s|master_ip|${MASTER_IP}|g" inventaire.ini
 ansible-playbook playbook.yml -i inventaire.ini --user $USER --extra-vars "ansible_sudo_pass=${PASS}"
+ansible-playbook roles/join-cluster/main.yml -i inventaire.ini --user $USER --extra-vars "ansible_sudo_pass=${PASS}"
 #replace inventory.ini
-mv inventaire.ini.backup inventaire.ini
+#mv inventaire.ini.backup inventaire.ini
